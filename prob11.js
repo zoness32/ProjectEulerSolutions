9efeacd7e69c97227f478375954777b7fe4913b3
Taylor Eernisse <taylor.eernisse@igrafx.com>
2020-11-02 18:49:26 -0800

    continued work on prob 11

--- prob11.js
+++ prob11.js
@@ -2,6 +2,11 @@ const answer = (grid, segmentLength) => {
     let array = grid.replace(/[ ]{2,}/g, ' ').split('\n');
     const gridHeight = array.length;
     const gridWidth = array[0].split(' ').length;
+
+    if (segmentLength * 2 > gridHeight * gridWidth) {
+        return "Error: segment length cannot be greater than one of the grid dimensions";
+    }
+
     array = array.join('').split(' ');
 
     const ltor = {
@@ -10,9 +15,9 @@ const answer = (grid, segmentLength) => {
 
     // const array = grid.replace(/\n+/g, ' ').replace(/[ ]{2,}/g, ' ').split(' ');
     let index = 0;
-    let rMostIndex = index + segmentLength % gridWidth > index ? index + segmentLength : undefined;
-    let lowestIndex = index + gridHeight * segmentLength < array.length ? index + gridHeight * segmentLength : undefined;
-
+    let rightIndex = index + segmentLength % gridWidth > index ? index + segmentLength : undefined;
+    let downIndex = index + gridHeight * segmentLength < array.length ? index + gridHeight * segmentLength : undefined;
+    let leftDiagIndex = index + (gridWidth * segmentLength) - (segmentLength - 1)
 }
 
 exports.largestProductInAGrid = answer;
